06c887fd5ac531d1c0c36b8ac5bfd79d3fb618d7
PDLOC <phamloc402@gmail.com>
2023-11-18 14:36:30 +0700

    .

--- OBooks/src/main/java/com/obooks/SecurityConfig.java
+++ OBooks/src/main/java/com/obooks/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		http.formLogin()
 			.loginPage("/security/login/form")
 			.loginProcessingUrl("/security/login")
-			.defaultSuccessUrl("/security/login/success",false)
+			.defaultSuccessUrl("/",false)
 			.failureUrl("/security/login/error");
 		
 		http.rememberMe()
