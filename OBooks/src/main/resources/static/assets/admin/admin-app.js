e8699ef2fff235b37725be0961f85114d25f6bc7
PDLOC <phamloc402@gmail.com>
2023-11-19 21:42:44 +0700

    update admin

--- OBooks/src/main/resources/static/assets/admin/admin-app.js
+++ OBooks/src/main/resources/static/assets/admin/admin-app.js
@@ -1,30 +1,26 @@
-app = angular.module("admin-app",["ngRoute"]);
+app = angular.module("admin-app", ["ngRoute"]);
 
 app.config(function($routeProvider) {
-    $routeProvider
-    .when("/product",{
-        templateUrl:"/assets/admin/doan/index.html",
-        controller: "product-ctrl"
-    })
-    .when("/account",{
-        templateUrl:"/assets/admin/card/index.html",
-        controller: "account-ctrl"
-    })
-    
-    .when("/authorize",{
-        templateUrl:"/assets/admin/table/index.html",
-        controller: "authority-ctrl"
-    })
-    .when("/unauthorized",{
-        templateUrl:"/assets/admin/authority/unauthorized.html",
-        controller: "authority-ctrl"
-    })
-    /*.otherwise({
-        redirectTo: "/summary",
-        controller: "summary-ctrl"
-    })*/
-    .otherwise({
-        redirectTo: "/account",
-        controller: "account-ctrl"
-    })
+	$routeProvider
+		.when("/product", {
+			templateUrl: "/assets/admin/product/index.html",
+			controller: "product-ctrl"
+		})
+		.when("/account", {
+			templateUrl: "/assets/admin/account/index.html",
+			controller: "account-ctrl"
+		})
+
+		.when("/authorize", {
+			templateUrl: "/assets/admin/authority/index.html",
+			controller: "authority-ctrl"
+		})
+		.when("/unauthorized", {
+			templateUrl: "/assets/admin/authority/unauthorized.html",
+			controller: "authority-ctrl"
+		})
+		.otherwise({
+			templateUrl: "/assets/admin/thongke/index.html",
+			controller: "thongke-ctrl"
+		})
 })
